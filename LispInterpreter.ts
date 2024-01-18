717cdc8c28cb9ace064b6eb6cb4dbcb07dedab8b
danielmckinnonanderson <danielmckinnon.anderson@gmail.com>
2024-01-18 07:37:01 -0600

    Initialize bun package

--- LispInterpreter.ts
+++ LispInterpreter.ts
@@ -3,41 +3,48 @@
 // And Michael Nielsen's blog post 'Lisp as the Maxwell's equations of software
 // Link: https://michaelnielsen.org/ddi/lisp-as-the-maxwells-equations-of-software/
 
+type _Symbol = string;
+type _Number = number;
+type _Atom = _Symbol | _Number;
+type _List = _Atom[];
+type _Expression = _Atom | _List;
+type _Environment = object;
+
 const Tokenize = (characters: string): string[] =>
   characters
     .replaceAll("(", " ( ")
     .replaceAll(")", " ) ")
     .split(" ")
-    .filter(token => token != "");
+    .filter((token: string ) => token != "");
 
-const ReadFromTokens = (tokens: string[]): Expression => {
+const ReadFromTokens = (tokens: string[]): _Expression => {
   if (tokens.length == 0) throw new Error("Syntax error: Unexpected EOF");
   
   const token = tokens.shift();
 
   if (token == "(") {
-    const result = [];
+    const result: any[] = [];
     while (tokens[0] != ")") result.push(ReadFromTokens(tokens));
     tokens.shift();
     return result;
   } 
   else if (token == ")") throw new Error("Syntax error: Unexpected ')'");
-  else return Atomize(token);
+  else return Atomize(token!);
 };
 
-const Atomize = (token: string): Atom => {
+const Atomize = (token: string): _Atom => {
   // Anything which is not a number is a symbol
-  if (isNaN(token)) return token as Symbol;
+  if (isNaN(token as any)) return token as _Symbol;
   
   // If token is not a symbol, try parsing it into number sub-types
   try {
     const integer = parseInt(token);
-    return integer as Number;
+    return integer as _Number;
   } catch (err: any) {}
   
   try {
     const float = parseFloat(token);
-    return float as Number;
+    return float as _Number;
   } catch (err: any) {}
 
   throw new Error(`Could not atomize the input token '${token}'`);
@@ -45,17 +52,12 @@ const Atomize = (token: string): Atom => {
 
 const Parse = (code: string) => ReadFromTokens(Tokenize(code));
 
-const Evaluate = (code: any) => {};
-
-type Symbol = string;
-type Number = number;
-type Atom = Symbol | Number;
-type List = Array;
-type Expression = Atom | List;
-type Environment = object;
+const Evaluate = (code: any) => { /* TODO */};
 
 
-const lispCode: string = "(begin (define r 10) (* pi (* r r)))";
+// Read lisp code from stdin
+const lispCode: string = Bun.argv[2];
+if (!lispCode) throw new Error("Found no Lisp code to interpret. Pass Lisp code or a path to a Lisp file to the program.");
 
 console.log(Parse(lispCode));
 
